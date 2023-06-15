3c138f57b186a761a988eed4e29f71acf56e665f
Yassinekhazine <yassinekhazine1@gmail.com>
2023-06-15 13:40:18 -0700

    Task_4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
-
 /**
-* main - Entry point
-*
-* Description: A Cprogram that ptints with put function
-*
-* Retyrn Always 0 (success)
-*/
-  
-int main(void)
-{ 
-               puts("\"Programming is like building a multilingual puzzle");
-               return (0);
-}    
+ * main- A program that prints a line ith puts function
+ * Return: 0 (Success)
+ */
+int main (void)
+{
+puts("\"Programing is like building a multingual puzzle");
+return (0);
+}
