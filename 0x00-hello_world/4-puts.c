a3b6f3378f3d6d1ef15b22ad480441557797007c
Yassinekhazine <yassinekhazine1@gmail.com>
2023-06-15 12:17:37 -0700

    Task_4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: A Cprogram that ptints with put function
+*
+* Retyrn Always 0 (success)
+*/
+  
+int main(void)
+{ 
+               puts("\"Programming is like building a multilingual puzzle");
+               return (0);
+}    
