b708eec82659d6751b8e22005b0a17b558b102d7
Yassinekhazine <yassinekhazine1@gmail.com>
2023-06-16 13:46:58 -0700

    Task_6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers from 0 to 9.
+ * Return: 0(success)
+ */
+int main(void)
+{
+int d;
+
+for (d = '0'; d <= '9'; d++)
+putchar(d);
+putchar('\n');
+return (0);
+}
