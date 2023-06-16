02545e9679be8c0e02772b57c8d8e9aaf152a7aa
Yassinekhazine <yassinekhazine1@gmail.com>
2023-06-16 14:21:18 -0700

    Task_8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print all numbers of base 16 in lowercase
+ * Return: 0(success)
+ */
+int main(void)
+{
+int d;
+char low;
+
+for (d = '0'; d <= '9'; d++)
+putchar(d);
+
+for (low = 'a'; low <= 'f'; low++)
+putchar(low);
+putchar('\n');
+return (0);
+}
