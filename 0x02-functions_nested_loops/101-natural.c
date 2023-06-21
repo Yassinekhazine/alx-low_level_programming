c2fead19d0f07f233168e4fdbc33853e675a67dd
Yassinekhazine <yassinekhazine1@gmail.com>
2023-06-20 18:58:33 -0700

    Task_13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-int sum, num;
+int num, sum = 0;
 for (num = 0; num < 1024; num++)
 {
 if ((num % 3 == 0) || (num % 5 == 0))
-sum + = num;
+sum += num;
 }
 printf("%d\n", sum);
 return (0);
