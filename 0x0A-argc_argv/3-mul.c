121e131e02eebed9885e4270d160de02d5ca69dd
Yassinekhazine <yassinekhazine1@gmail.com>
2023-07-07 10:13:19 -0700

    task_3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,23 +4,24 @@
 /**
  * main-print sum of 2 numbers
  * @argc: number of command line arguments
- * @argv: pointer to an array of command line arguments
- * Return: 0(success)
+ * @argv: pointer to an array of command line argument
+ *  Return: If the program receives two arguments print 0
+ *  If the program does not receive two arguments print 1
  */
 
 int main(int argc, char *argv[])
 {
-int sum;
+int num1, num2, sum;
 
-if (argc == 3)
-{
-sum = atoi(argv[1]) * atoi(argv[2]);
-printf("%d\n", sum);
-}
-else
+if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
+
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+sum = num1 *num2;
+printf("%d\n", sum);
 return (0);
 }
